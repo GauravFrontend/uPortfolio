dc5afe6d6290deab62a44c1212f1ac6fd523c971
GauravFrontend <gaurav9911093928@gmail.com>
2024-02-27 02:45:59 +0530

    new portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+import Structure from './components/landing/Structure'
+
+function App() {
+
+
+  return (
+    <div className="dark:bg-primary bg-zinc-200 ">
+    <Structure/>
+    </div>
+  )
+}
+
+export default App
