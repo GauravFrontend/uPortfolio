c91113df29ea4274f856232e3744d010b4df21ee
GauravFrontend <gaurav9911093928@gmail.com>
2024-03-01 06:52:15 +0530

    small fix

--- src/components/landing/Structure.jsx
+++ src/components/landing/Structure.jsx
@@ -345,7 +345,7 @@ const Structure = () => {
 
                                     <button
                                         onClick={() => toast.success("You're awesome!")}
-                                        className="text-sm py-2 w-full px-4 h-12 font-semibold focus:ring-2 rounded-lg bg-primary dark:bg-white dark:text-primary dark:hover:text-white hover:text-primary dark:hover:bg-white/5 hover:bg-primary/10 text-white flex duration-200 focus:ring-offset-2 focus:ring-inline-flex items-center justify-between"
+                                        className="text-sm py-2 w-full px-4 h-12 font-semibold focus:ring-2 rounded-lg bg-primary dark:bg-white dark:text-primary dark:hover:text-white hover:text-primary dark:hover:bg-white/5 hover:bg-primary/10 text-white flex duration-200 focus:ring-offset-2 focus:ring-inline-flex items-center justify-center"
                                     >Click here
                                     </button>
                                 </div>
